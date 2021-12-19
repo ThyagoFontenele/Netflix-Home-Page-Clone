94f4df297600725306e4c7ba72ccfbe763075618
thyago_fontenele <thyago.fontenele@injunior.com.br>
2021-12-19 18:26:48 -0300

    pegando informacoes do filme em destaque

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import React, { useEffect, useState } from 'react'
 import './App.css';
 import Tmdb from './Tmdb'
 import MovieRow from './components/MovieRow/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie/FeaturedMovie';
 
 export default function App(){
+
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
   useEffect(()=>{
     const loadAll = async ()=>{
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+      
+      let originals = list.filter( e => e.slug === 'originals' );
+      let randomNum = Math.floor(Math.random() * (originals[0].items.results.length) );
+      let chosen = originals[0].items.results[randomNum];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
     loadAll();
   }, [])
-  console.log(movieList)
+
   return(
     <>
       <header></header>
+
+      
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
 
       <section>
         {movieList.map((item, key) => (
---
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './FeaturedMovie.module.css'
+
+export default function FeaturedMovie() {
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
