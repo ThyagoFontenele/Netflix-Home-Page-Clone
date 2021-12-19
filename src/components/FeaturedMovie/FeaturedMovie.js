9d82d87d5bea0b85d88d82ba6391405be48d3156
thyago_fontenele <thyago.fontenele@injunior.com.br>
2021-12-19 20:06:47 -0300

    todos os dados do destaque adquiridos

--- src/components/FeaturedMovie/FeaturedMovie.js
+++ src/components/FeaturedMovie/FeaturedMovie.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import classes from './FeaturedMovie.module.css'
 
-export default function FeaturedMovie() {
+export default function FeaturedMovie({item}) {
 
+    const [point, setPoint ] = useState(0);
+    const [textSeasons, setTextSeasons] = useState('');
+
+    let date = new Date(item.first_air_date);
+    let genres = [];
+
+    for(let i in item.genres){
+        genres.push(item.genres[i].name);
+    }
+
+
+    useEffect(() => {
+        let relevant = Number(item.vote_average)*10;
+
+        let seasons = item.number_of_seasons;
+
+        if(seasons === 1){
+            setTextSeasons(`${seasons} temporada`);
+        }else{
+            setTextSeasons(`${seasons} temporadas`);
+        }
+        setPoint(relevant)
+
+    }, [])
+    console.log(item)
     return(
-        <div>
-            
-        </div>
+        <section className={classes.featured} style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`,
+
+            }} >
+
+            <div className={classes.featured_vertical}>
+                <div className={classes.featured_horizontal}>
+                    <div className={classes.featured_name}>{item.original_name}</div>
+                    <div className={classes.featured_info}>
+                        <div className={classes.featured_points}> {point}% de relevância </div>
+                        <div className={classes.featured_year}>{date.getFullYear()}</div>
+                        <div className={classes.featured_seasons}>{textSeasons}</div>
+                    </div>
+                    <div className={classes.featured_description}>{item.overview}</div>
+                    <div className={classes.featured_buttons}></div>
+                    <div className={classes.featured_genres}><strong>Gêneros:</strong> {genres.join(", ")}</div>
+                </div>
+            </div>
+        </section >
     )
 }
